f1213846a376b0bf1ef2f177fe6006385e548f79
Parm_inder <149635583+parminder05@users.noreply.github.com>
2026-03-30 01:44:52 +0530

    Initial commit - India States Quiz Game using Turtle and Pandas

---
+++ main.py
@@ -0,0 +1,42 @@
+import turtle
+
+screen=turtle.Screen()
+image="indian_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# def coor(x,y):
+# 	print(x,y)
+
+# turtle.onscreenclick(coor)
+# turtle.mainloop()
+
+
+import pandas 
+data = pandas.read_csv("states_data.csv")
+all_states=data.state.to_list()
+
+
+
+missed_state=[]
+guessed_state=[]
+while len(guessed_state) < 28:
+	answer=screen.textinput(title="Guess the state", prompt="What is the another state's name").title()
+
+	if answer == "Exit":
+		for miss in all_states:
+			if miss not in guessed_state:
+				missed_state.append(miss)
+		to_learn=pandas.DataFrame(missed_state)
+		to_learn.to_csv("States_To_Learn")
+
+		break 
+	if answer in all_states:
+		guessed_state.append(answer)
+		t=turtle.Turtle()
+		t.hideturtle()
+		t.penup()
+		state_data=data[data.state==answer]
+		t.goto(state_data.x.item(), state_data.y.item())
+		t.write(state_data.state.item(), font=("Arial", 10, "bold"))
+
